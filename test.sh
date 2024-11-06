8257ce2a246176a288aa96c6b845e9cfb5515a49
bledsoef <bledsoef@berea.edu>
2024-11-05 21:35:04 -0500

    finished questions and updated solution

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 for i in $(seq 1 3000);
 do
-    ./tlb $i 200000
+    ./tlb $i 2000
 done
--- tlb.c
+++ tlb.c
@@ -9,65 +9,48 @@ Questions:
 
 5. I did this by just creating a new loop that iterates through the array we just messed with and adds all those values to another variable.
 
-6. 
+6. By tying it to one core it makes sure there is cache affinity and that the process is not getting bounced from core to core and needing to start with a fresh core. I attempted to implement it and had no such luck. You can include a package to specify the core and make sure the process is only running on one CPU with using things like pthread_setaffinity_np. I tried to implement this but the resources online either did not work or were very complicated.
 
-7.
+7. I inititally was initializing the array just by doing int a[NUMPAGES * jump]; but I shifted to using calloc that way it is already cleared and allocated.
 
 */
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
-#include <pthread.h>
-#include <sched.h>
+#include <unistd.h>
 
-void *timed_function(void *arg) {
-    int PAGESIZE = 40;
-    long int NUMPAGES = (long int) atoi(((char **)arg)[1]);
-    long int TRIALS = (long int) atoi(((char **)arg)[2]);
-    int jump = PAGESIZE / sizeof(int);
-    int a[NUMPAGES * jump];
-    int seconds = 0;
+int main(int argc, char *argv[])
+{
+    long int NUMPAGES = (long int)atoi(argv[1]);
+    long int TRIALS = (long int)atoi(argv[2]);
+    long PAGESIZE = sysconf(_SC_PAGESIZE); // 163840 is the default page size for MACOS
+    long jump = PAGESIZE / sizeof(int); 
+    int *a = calloc(NUMPAGES, PAGESIZE);
     int counter = 0;
     int microseconds = 0;
     struct timeval start;
     struct timeval end;
-
+    gettimeofday(&start, NULL);
     for (int j = 0; j < TRIALS; j += 1)
     {
-        gettimeofday(&start, NULL);
         for (int i = 0; i < NUMPAGES * jump; i += jump)
         {
             a[i] += 1;
         }
-        gettimeofday(&end, NULL);
-        if (end.tv_sec - start.tv_sec == 0) {
-            microseconds += (end.tv_usec - start.tv_usec);
-            counter += 1;
-        }
     }
+    gettimeofday(&end, NULL);
 
-    volatile int result = 0;
+    int result = 0; // trying to maybe prevent compiler optimization?
     for (int i = 0; i < NUMPAGES * jump; i += jump) {
         result += a[i];
     }
 
-    int *var = a;
-    printf("%d\n", (int) ((microseconds + result - result) / counter));
-    return NULL;
-}
+    int *var = a; 
+    printf("%ld, %f\n", NUMPAGES,
+           ((end.tv_sec - start.tv_sec) * 1E6 // convert seconds to micro
+            + end.tv_usec - start.tv_usec) /
+               (TRIALS * NUMPAGES)); // calculte per access
 
-int main(int argc, char *argv[]) {
-    pthread_t thread;
-    cpu_set_t cpuset;
-    int core_id = 0; 
-
-    pthread_create(&thread, NULL, timed_function, argv);
-
-    CPU_ZERO(&cpuset);
-    CPU_SET(core_id, &cpuset); 
-    pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
-
-    pthread_join(thread, NULL);
-
+    free(a);
     return 0;
 }
